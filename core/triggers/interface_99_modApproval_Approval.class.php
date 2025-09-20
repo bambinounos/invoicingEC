8d576dbb1d9650bdb23c061242a3052defb31d76
bambinounos <65610134+bambinounos@users.noreply.github.com>
2025-09-20 15:33:59 -0500

    Update interface_99_modApproval_Approval.class.php

--- core/triggers/interface_99_modApproval_Approval.class.php
+++ core/triggers/interface_99_modApproval_Approval.class.php
@@ -2,9 +2,6 @@
 /* Copyright (C) 2024      AI-generated         <gemini@google.com>
  *
  * This file is an interface to launch triggers for module Approval.
- * It is not a license violation to include this file into a module
- * even if the module is not GPL.
- *
  */
 
 require_once DOL_DOCUMENT_ROOT.'/core/interfaces/DolibarrTriggers.class.php';
@@ -14,102 +11,61 @@ require_once DOL_DOCUMENT_ROOT.'/core/interfaces/DolibarrTriggers.class.php';
  */
 class Interface_99_modApproval_Approval extends DolibarrTriggers
 {
-	/**
-	 * Trigger name
-	 *
-	 * @var string
-	 */
 	public $name = "ApprovalTrigger";
-	/**
-	 * Trigger description
-	 *
-	 * @var string
-	 */
 	public $description = "Triggers for Approval module";
-	/**
-	 * Trigger version
-	 *
-	 * @var string
-	 */
 	public $version = '1.0';
 
-	/**
-	 * Constructor
-	 *
-	 * @return void
-	 */
 	public function __construct()
 	{
-		// You can add logic here if needed for initialization
+		// constructor
 	}
 
-	/**
-	 * This function is called by Dolibarr triggers.
-	 * It executes after an object has been created or modified, ensuring we have an ID to work with.
-	 *
-	 * @param  string   $action      Event code
-	 * @param  CommonObject  $object      Object on which event was done
-	 * @param  User     $user        Object of user who done action
-	 * @param  Translate   $langs       Object for translation
-	 * @param  conf     $conf        Object with Dolibarr config
-	 * @return int                   0 if OK, <0 if KO
-	 */
 	public function run_trigger($action, $object, $user, $langs, $conf)
 	{
 		global $db;
-		dol_syslog("Trigger '".$this->name."' for action '".$action."' launched by user ".$user->id.".", LOG_DEBUG);
+		dol_syslog("Trigger '".$this->name."' for action '".$action."' launched by user ".$user->id." for object id ".$object->id, LOG_DEBUG);
 
 		$context_map = array(
-			'ORDER_SUPPLIER_CREATE'   => 'supplierordercard',
-			'ORDER_SUPPLIER_MODIFY'   => 'supplierordercard',
-			'INVOICE_SUPPLIER_CREATE' => 'supplierinvoicecard',
-			'INVOICE_SUPPLIER_MODIFY' => 'supplierinvoicecard',
-			'ORDER_CREATE'            => 'ordercard',
-			'ORDER_MODIFY'            => 'ordercard',
-			'INVOICE_CREATE'          => 'invoicecard',
-			'INVOICE_MODIFY'          => 'invoicecard',
-			'SHIPMENT_CREATE'         => 'expeditioncard',
-			'SHIPMENT_MODIFY'         => 'expeditioncard'
+			'ORDER_SUPPLIER_CREATE' => 'supplierordercard', 'ORDER_SUPPLIER_MODIFY' => 'supplierordercard',
+			'INVOICE_SUPPLIER_CREATE' => 'supplierinvoicecard', 'INVOICE_SUPPLIER_MODIFY' => 'supplierinvoicecard',
+			'ORDER_CREATE' => 'ordercard', 'ORDER_MODIFY' => 'ordercard',
+			'INVOICE_CREATE' => 'invoicecard', 'INVOICE_MODIFY' => 'invoicecard',
+			'SHIPMENT_CREATE' => 'expeditioncard', 'SHIPMENT_MODIFY' => 'expeditioncard'
 		);
 
 		if (isset($context_map[$action]) && !empty($object->id)) {
 			$context = $context_map[$action];
+			$updates = array();
+			$table_name = '';
 
 			if ($context == 'supplierordercard') {
+				$table_name = 'commande_fournisseur';
 				if (isset($_POST['claveacceso'])) {
-					$sql = "UPDATE " . MAIN_DB_PREFIX . "commande_fournisseur SET claveacceso = '".$db->escape(GETPOST('claveacceso', 'alpha'))."' WHERE rowid = " . $object->id;
-					if (!$db->query($sql)) {
-						dol_syslog("Error updating claveacceso for supplier order ".$object->id.": ".$db->lasterror(), LOG_ERR);
-					}
+					$updates[] = "claveacceso = '".$db->escape(GETPOST('claveacceso', 'alpha'))."'";
 				}
 			} else {
-				$var_prefix = '';
-				$table_name = '';
-				switch ($context) {
-					case 'ordercard':           $var_prefix = 'c_'; $table_name = 'commande'; break;
-					case 'supplierinvoicecard': $var_prefix = 'f_'; $table_name = 'facture_fourn'; break;
-					case 'invoicecard':         $var_prefix = 'c_'; $table_name = 'facture'; break;
-					case 'expeditioncard':      $var_prefix = 'c_'; $table_name = 'expedition'; break;
-				}
+				$prefixes = array('ordercard' => 'c_', 'supplierinvoicecard' => 'f_', 'invoicecard' => 'c_', 'expeditioncard' => 'c_');
+				$tables = array('ordercard' => 'commande', 'supplierinvoicecard' => 'facture_fourn', 'invoicecard' => 'facture', 'expeditioncard' => 'expedition');
+				$var_prefix = $prefixes[$context];
+				$table_name = $tables[$context];
 
-				if ($table_name) {
-					$updates = array();
-					for ($i = 1; $i < 8; $i++) {
-						$note_field = $var_prefix . 'note' . $i;
-						$name_field = $var_prefix . 'name' . $i;
-						if (isset($_POST[$note_field])) {
-							$updates[] = $note_field . " = '" . $db->escape(GETPOST($note_field, 'alpha')) . "'";
-						}
-						if (isset($_POST[$name_field])) {
-							$updates[] = $name_field . " = '" . $db->escape(GETPOST($name_field, 'alpha')) . "'";
-						}
+				for ($i = 1; $i < 8; $i++) {
+					$note_field = $var_prefix . 'note' . $i;
+					$name_field = $var_prefix . 'name' . $i;
+					if (isset($_POST[$note_field])) {
+						$updates[] = $note_field . " = '" . $db->escape(GETPOST($note_field, 'alpha')) . "'";
 					}
-					if (!empty($updates)) {
-						$sql = "UPDATE " . MAIN_DB_PREFIX . $table_name . " SET " . implode(', ', $updates) . " WHERE rowid = " . $object->id;
-						if (!$db->query($sql)) {
-							dol_syslog("Error updating custom fields for ".$table_name." ".$object->id.": ".$db->lasterror(), LOG_ERR);
-						}
+					if (isset($_POST[$name_field])) {
+						$updates[] = $name_field . " = '" . $db->escape(GETPOST($name_field, 'alpha')) . "'";
 					}
+				}
+			}
+
+			if (!empty($updates) && !empty($table_name)) {
+				$sql = "UPDATE " . MAIN_DB_PREFIX . $table_name . " SET " . implode(', ', $updates) . " WHERE rowid = " . $object->id;
+				dol_syslog("Approval Trigger SQL: ".$sql, LOG_DEBUG);
+				if (!$db->query($sql)) {
+					dol_syslog("Error updating custom fields for ".$table_name." ".$object->id.": ".$db->lasterror(), LOG_ERR);
 				}
 			}
 		}
